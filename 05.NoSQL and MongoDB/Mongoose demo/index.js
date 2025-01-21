f57dabc84e5e8d0d517f01763445eaae2ad0578c
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-01-21 20:27:12 +0000

    connected with atlas

--- 05.NoSQL and MongoDB/Mongoose demo/index.js
+++ 05.NoSQL and MongoDB/Mongoose demo/index.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
+const url='mongodb://localhost:27017/studentsDb'
+
+const uri ='mongodb+srv://mgeorgieva95@cluster0.3frn6.mongodb.net/studentsDb?retryWrites=true&w=majority&appName=Cluster0'
+
 try {
+    await mongoose.connect(uri);
     await mongoose.connect('mongodb://localhost:27017/studentsDb');
     console.log('Connected to DB');
     
@@ -8,4 +13,3 @@ try {
     console.log('Failed to connect DB ...');
     console.log(err);
 }
-
