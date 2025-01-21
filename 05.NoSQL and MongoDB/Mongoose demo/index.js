d366ebe8ea1e29844ede7d24f716a0a882f6b398
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-01-21 10:39:34 +0000

    connect through mongoose

---
+++ 05.NoSQL and MongoDB/Mongoose demo/index.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+try {
+    await mongoose.connect('mongodb://localhost:27017/studentsDb');
+    console.log('Connected to DB');
+    
+} catch (err) {
+    console.log('Failed to connect DB ...');
+    console.log(err);
+}
