a8bb7a1e7527394a7b4434242d596a2199fe4758
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-01-21 21:33:51 +0000

    mongoose Schema and model

--- 05.NoSQL and MongoDB/Mongoose demo/index.js
+++ 05.NoSQL and MongoDB/Mongoose demo/index.js
@@ -1,15 +1,34 @@
-import mongoose from "mongoose";
+import mongoose, {Schema, model} from "mongoose";
 
 const url='mongodb://localhost:27017/studentsDb'
 
-const uri ='mongodb+srv://mgeorgieva95@cluster0.3frn6.mongodb.net/studentsDb?retryWrites=true&w=majority&appName=Cluster0'
-
 try {
-    await mongoose.connect(uri);
-    await mongoose.connect('mongodb://localhost:27017/studentsDb');
+    await mongoose.connect(url);
     console.log('Connected to DB');
     
 } catch (err) {
     console.log('Failed to connect DB ...');
     console.log(err);
 }
+
+//Setup mongoose schema
+const studentSchema = new Schema({
+    name: String,
+    age: Number
+});
+
+//Create mongoose model
+const Student = model('Student', studentSchema);
+//=> collection students
+
+//Query all data from model => array of document objects
+const students = await Student.find();
+console.log(students);
+
+//Filter
+const filteredStudents=await Student.find({age:20});
+
+//Filter with Operators
+const filteredStudents2=await Student.find({age: {$ne: 20}});
+console.log(filteredStudents2);
+
