68c086f0d67372353fd1688094a6166681cc86e8
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-01-28 22:33:05 +0000

    sessions in express-session library

--- 07.Session and Authentication/index.js
+++ 07.Session and Authentication/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import expressSession from "express-session";
 
 const app = express();
 
 app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: "SecretSE",
+    resave: false, //resave only after modify
+    saveUninitialized: true,
+    cookie: { secure: false }, //true for HTTPS
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("It works");
@@ -26,13 +35,31 @@ app.get("/get-cookie-manually", (req, res) => {
 
 //Using cookie-parser library
 app.get("/set-cookie", (req, res) => {
-  res.cookie("username", "Jane");
+  res.cookie("username", "Jane", {
+    httpOnly: true,
+  });
   res.end();
 });
 
 app.get("/get-cookie", (req, res) => {
   const cookie = req.cookies["username"];
   console.log(cookie);
+  res.end();
+});
+
+//express-session library, sets unique id on the client and keeps the information on the server
+app.get("/set-session-data/:name", (req, res) => {
+  req.session.username = req.params.name;
+  req.session.age = 20;
+
+  res.end();
+});
+
+app.get("/get-session-data", (req, res) => {
+  console.log(req.session);
+  console.log(req.session.username);
+  res.send(req.session.username);
+
   res.end();
 });
 
