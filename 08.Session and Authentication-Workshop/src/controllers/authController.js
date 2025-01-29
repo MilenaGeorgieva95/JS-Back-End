1888afa30d5ab051d63245b88faa8c2e6d9b07fb
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-01-29 22:28:13 +0000

    add login and register page

--- 08.Session and Authentication-Workshop/src/controllers/authController.js
+++ 08.Session and Authentication-Workshop/src/controllers/authController.js
@@ -2,6 +2,12 @@ import {Router} from 'express';
 
 const authController= Router();
 
+authController.get('/register', (req, res)=>{
+    res.render('auth/register')
+});
 
+authController.get('/login', (req, res)=>{
+    res.render('auth/login')
+});
 
 export default authController;
