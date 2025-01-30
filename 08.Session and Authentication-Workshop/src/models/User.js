b4c83bcfcc8a78f38582e140bc39bddaeb96dc0f
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-01-30 10:01:45 +0000

    hash password pre save

--- 08.Session and Authentication-Workshop/src/models/User.js
+++ 08.Session and Authentication-Workshop/src/models/User.js
@@ -1,10 +1,19 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema=new Schema({
-    email: String,
-    password: String
+const userSchema = new Schema({
+  email: String,
+  password: String,
 });
 
-const User = model('User', userSchema);
+//pre validate post => pre save post
+userSchema.pre('save', async function (){
+    console.log(this._id)
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = model("User", userSchema);
 
 export default User;
+
+
