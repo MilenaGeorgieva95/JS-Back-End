ccaa8bbe2402a0af85c9c996ae00d5473d1185e9
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-01-31 09:34:14 +0000

    send cookie token to client on login

--- 08.Session and Authentication-Workshop/src/controllers/authController.js
+++ 08.Session and Authentication-Workshop/src/controllers/authController.js
@@ -20,14 +20,13 @@ authController.get("/login", (req, res) => {
 authController.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
-    const token=await authService.login(email, password);
-    console.log(token);
+    const token = await authService.login(email, password);
+    res.cookie("auth", token);
+    res.redirect("/");
   } catch (err) {
     console.log(err);
-    return res.redirect("/404");
+    res.redirect("/404");
   }
-
-  res.redirect("/");
 });
 
 export default authController;
