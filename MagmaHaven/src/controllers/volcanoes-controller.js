a6b01feb3e0d003f56708ba53504b84403e5df1b
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-07 21:06:51 +0000

    add isOwner check

--- MagmaHaven/src/controllers/volcanoes-controller.js
+++ MagmaHaven/src/controllers/volcanoes-controller.js
@@ -3,7 +3,6 @@ import { isNotLoggedIn } from "../middlewares/auth-middleware.js";
 import volcanoesService from "../services/volcanoes-service.js";
 import { getErrorMessage } from "../utils/errorUtils.js";
 import { getTypes } from "../utils/categoriesUtils.js";
-import { Types } from "mongoose";
 
 const volcanoesController = Router();
 
@@ -44,11 +43,13 @@ volcanoesController.get("/", async (req, res) => {
 volcanoesController.get("/:volcanoId/details", async (req, res) => {
   const id = req.params.volcanoId;
   try {
-    const volcano = await volcanoesService.getOneById(id).lean()
-    volcano.votesCount = volcano.voteList?.length||0;
+    const volcano = await volcanoesService.getOneById(id).lean();
+    volcano.votesCount = volcano.voteList?.length || 0;
     const isUser = !!req.user;
     const isOwner = volcano.owner.toString() === req.user?._id;
-    const isVoted = volcano.voteList.some(userId=>userId.toString()===req.user?._id);
+    const isVoted = volcano.voteList?.some(
+      (userId) => userId.toString() === req.user?._id
+    );
 
     res.render("volcanoes/details", {
       volcano,
@@ -67,7 +68,14 @@ volcanoesController.get("/:volcanoId/vote", isNotLoggedIn, async (req, res) => {
   const volcanoId = req.params.volcanoId;
   const userId = req.user._id;
   try {
-    await volcanoesService.vote(volcanoId, userId);
+    const isOwner = await volcanoesService.isOwner(volcanoId, userId);
+    if (isOwner) {
+      return res.redirect(`/volcanoes/${volcanoId}/details`);
+    }
+    const hasVoted=await volcanoesService.hasVoted(volcanoId, userId);
+    if(!hasVoted){
+      await volcanoesService.vote(volcanoId, userId);
+    }
     res.redirect(`/volcanoes/${volcanoId}/details`);
   } catch (err) {
     const error = getErrorMessage(err);
@@ -75,53 +83,70 @@ volcanoesController.get("/:volcanoId/vote", isNotLoggedIn, async (req, res) => {
   }
 });
 
-volcanoesController.get("/:volcanoId/delete", isNotLoggedIn, async (req, res) => {
-  const volcanoId = req.params.volcanoId;
-  //TODO isOwner?
-  const userId = req.user._id;
-  try {
-    await volcanoesService.del(volcanoId);
-    res.redirect(`/volcanoes`);
-  } catch (err) {
-    const error = getErrorMessage(err);
-    res.redirect("/404", { error });
+volcanoesController.get(
+  "/:volcanoId/delete",
+  isNotLoggedIn,
+  async (req, res) => {
+    const volcanoId = req.params.volcanoId;
+    const userId = req.user._id;
+
+    try {
+      const isOwner = await volcanoesService.isOwner(volcanoId, userId);
+      if (!isOwner) {
+        return res.redirect(`/volcanoes/${volcanoId}/details`);
+      }
+      await volcanoesService.del(volcanoId);
+      res.redirect(`/volcanoes`);
+    } catch (err) {
+      const error = getErrorMessage(err);
+      res.redirect("/404", { error });
+    }
   }
-});
+);
 
 volcanoesController.get("/:volcanoId/edit", isNotLoggedIn, async (req, res) => {
   const volcanoId = req.params.volcanoId;
-  //TODO isOwner?
   const userId = req.user._id;
   try {
-    const isOwner = await volcanoesService.isOwner(volcanoId,userId);
-    if(!isOwner){
-      return res.redirect('/404')
+    const isOwner = await volcanoesService.isOwner(volcanoId, userId);
+    if (!isOwner) {
+      return res.redirect("/404");
     }
-   const volcano = await volcanoesService.getOneById(volcanoId).lean();
-   const types=getTypes(volcano.typeVolcano)
-    res.render('volcanoes/edit', {title: 'Edit Page', volcano, types})
+    const volcano = await volcanoesService.getOneById(volcanoId).lean();
+    const types = getTypes(volcano.typeVolcano);
+    res.render("volcanoes/edit", { title: "Edit Page", volcano, types });
   } catch (err) {
     const error = getErrorMessage(err);
     res.redirect("/404", { error });
   }
-
 });
 
-volcanoesController.post("/:volcanoId/edit", isNotLoggedIn, async (req, res) => {
-  const volcanoId = req.params.volcanoId;
-  //TODO isOwner?
-  const userId = req.user._id;
-  const volcanoData=req.body;
-  
-  try {
-    await volcanoesService.edit(volcanoId, volcanoData);
-    res.redirect(`/volcanoes/${volcanoId}/details`);
-  } catch (err) {
-    const error = getErrorMessage(err);
-    const types=getTypes(volcanoData.typeVolcano)
-    res.render("volcanoes/edit", { error, title: 'Edit Page', volcano: volcanoData, types });
+volcanoesController.post(
+  "/:volcanoId/edit",
+  isNotLoggedIn,
+  async (req, res) => {
+    const volcanoId = req.params.volcanoId;
+    const userId = req.user._id;
+    const volcanoData = req.body;
+
+    try {
+      const isOwner = await volcanoesService.isOwner(volcanoId, userId);
+      if (!isOwner) {
+        return res.redirect("/404");
+      }
+      await volcanoesService.edit(volcanoId, volcanoData);
+      res.redirect(`/volcanoes/${volcanoId}/details`);
+    } catch (err) {
+      const error = getErrorMessage(err);
+      const types = getTypes(volcanoData.typeVolcano);
+      res.render("volcanoes/edit", {
+        error,
+        title: "Edit Page",
+        volcano: volcanoData,
+        types,
+      });
+    }
   }
-});
-
+);
 
 export default volcanoesController;
--- MagmaHaven/src/services/volcanoes-service.js
+++ MagmaHaven/src/services/volcanoes-service.js
@@ -5,33 +5,44 @@ async function createVolcano(data, user) {
   return Volcano.create(data);
 }
 
-function getAll(){
+function getAll() {
   return Volcano.find({});
 }
 
-function getOneById(id){
-  return Volcano.findById(id)
+function getOneById(id) {
+  return Volcano.findById(id);
 }
 
-function vote(volcanoId, userId){
-  return Volcano.findByIdAndUpdate({_id: volcanoId}, {$push: {voteList: userId}}, {runValidators: true})
+function vote(volcanoId, userId) {
+  return Volcano.findByIdAndUpdate(
+    { _id: volcanoId },
+    { $push: { voteList: userId } },
+    { runValidators: true }
+  );
 }
 
-function del(volcanoId){
-  return Volcano.findByIdAndDelete(volcanoId)
+function del(volcanoId) {
+  return Volcano.findByIdAndDelete(volcanoId);
 }
 
 //!{runValidators: true} for .findByIdAndUpdate()
-function edit(volcanoId, volcanoData){
-  return Volcano.findByIdAndUpdate(volcanoId, volcanoData, {runValidators: true})
+function edit(volcanoId, volcanoData) {
+  return Volcano.findByIdAndUpdate(volcanoId, volcanoData, {
+    runValidators: true,
+  });
 }
 
-async function isOwner(volcanoId, userId){
-  const volcano= await Volcano.findById(volcanoId).lean();
-  const volcanoOwnerId=volcano.owner;
-  return volcanoOwnerId.toString()===userId
-  }
+async function isOwner(volcanoId, userId) {
+  const volcano = await Volcano.findById(volcanoId).lean();
+  const volcanoOwnerId = volcano.owner;
+  return volcanoOwnerId.toString() === userId;
+}
 
+async function hasVoted(volcanoId, userId) {
+  const volcano = await Volcano.findById(volcanoId).lean();
+  const hasVoted = volcano.voteList?.some((vote) => vote.toString() === userId);
+  return hasVoted;
+}
 
 const volcanoesService = {
   createVolcano,
@@ -40,7 +51,8 @@ const volcanoesService = {
   vote,
   del,
   edit,
-  isOwner
+  isOwner,
+  hasVoted,
 };
 
 export default volcanoesService;
