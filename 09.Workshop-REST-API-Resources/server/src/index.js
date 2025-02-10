1fd2cc5bd099019dfc82f8447aa965f08206e293
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-10 10:50:11 +0000

    Update index.js

--- 09.Workshop-REST-API-Resources/server/src/index.js
+++ 09.Workshop-REST-API-Resources/server/src/index.js
@@ -4,7 +4,9 @@ import routes from "./routes.js";
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 app.use(routes);
