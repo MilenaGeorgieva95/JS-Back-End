a7f09e159f93dc2c239095dc2f2432cbc2332979
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-02-17 22:06:55 +0000

    add edit, delete, update functionality

--- 09.Workshop-REST-API-Resources/server/src/controllers/furnitureController.js
+++ 09.Workshop-REST-API-Resources/server/src/controllers/furnitureController.js
@@ -4,16 +4,17 @@ import furnitureService from "../services/furnitureService.js";
 const furnitureController = Router();
 
 furnitureController.get("/", async (req, res) => {
-  const allFurniture = await furnitureService.getAll();
+  const query = req.query;
+  const allFurniture = await furnitureService.getAll(query);
   res.json(allFurniture);
 });
 
 furnitureController.post("/", async (req, res) => {
   const furnitureData = req.body;
-  console.log(req.header('x-authorization'));
+  console.log(req.header("x-authorization"));
 
-  const userId=req.user._id
-  furnitureData._ownerId=userId;
+  const userId = req.user._id;
+  furnitureData._ownerId = userId;
 
   const newItem = await furnitureService.create(furnitureData);
   res.json(newItem);
@@ -23,6 +24,22 @@ furnitureController.get("/:furnitureId", async (req, res) => {
   const furnitureId = req.params.furnitureId;
   const furniture = await furnitureService.getOne(furnitureId);
   res.json(furniture);
+});
+
+furnitureController.delete("/:furnitureId", async (req, res) => {
+  const furnitureId = req.params.furnitureId;
+  await furnitureService.del(furnitureId);
+  res.status(204).end();
+});
+
+furnitureController.put("/:furnitureId", async (req, res) => {
+  const furnitureId = req.params.furnitureId;
+  const furnitureData = req.body;
+  const newFurniture = await furnitureService.update(
+    furnitureId,
+    furnitureData
+  );
+  res.json(newFurniture);
 });
 
 export default furnitureController;
--- 09.Workshop-REST-API-Resources/server/src/services/furnitureService.js
+++ 09.Workshop-REST-API-Resources/server/src/services/furnitureService.js
@@ -1,15 +1,29 @@
 import Furniture from "../models/Furniture.js";
+import querystring from "querystring";
 
 const furnitureService = {
-  getAll() {
-    return Furniture.find({});
+  getAll(filter = {}) {
+    const query = Furniture.find({});
+    if (filter.where) {
+      // const ownerId = filter.where.slice(10, -1);
+      const qs = querystring.parse(filter.where.replaceAll('"', ""));
+      // qs = _ownerId: ididid123
+      query.find(qs);
+    }
+    return query;
   },
   create(furnitureData) {
     return Furniture.create(furnitureData);
   },
-  getOne(furnitureId){
-    return Furniture.findById(furnitureId)
-  }
+  getOne(furnitureId) {
+    return Furniture.findById(furnitureId);
+  },
+  del(furnitureid) {
+    return Furniture.findByIdAndDelete(furnitureid);
+  },
+  update(furnitureId, furnitureData) {
+    return Furniture.findByIdAndUpdate(furnitureId, furnitureData);
+  },
 };
 
 export default furnitureService;
